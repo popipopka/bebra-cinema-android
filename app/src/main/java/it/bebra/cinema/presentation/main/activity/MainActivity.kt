d25f0c73f0180a7ecfac9bee1c1f992b714e7c25
popipopka <eg.belykh@yandex.ru>
2024-10-20 20:30:18 +0300

    init

---
+++ app/src/main/java/it/bebra/cinema/presentation/main/activity/MainActivity.kt
@@ -0,0 +1,10 @@
+package it.bebra.cinema.presentation.main.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
