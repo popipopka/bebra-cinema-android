342da36104fafaae4b2c586cce21637e23977e82
popipopka <eg.belykh@yandex.ru>
2024-10-20 21:20:21 +0300

    feat (app): add login activity
    - Создан activity_login.xml
    - Изменена стандартная тема в themes.xml
    - Добавлены статические строки в strings.xml для отображения в login activity
    - Созданы иконки, использующиеся в activity_login.xml
    - Создан класс LoginActivity
    - В MainActivity добавлен запуск LoginActivity как временное решение
    
    Signed-off-by: popipopka <eg.belykh@yandex.ru>

---
+++ app/src/main/java/it/bebra/cinema/presentation/login/activity/LoginActivity.kt
@@ -0,0 +1,12 @@
+package it.bebra.cinema.presentation.login.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import it.bebra.cinema.R
+
+class LoginActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
--- app/src/main/java/it/bebra/cinema/presentation/main/activity/MainActivity.kt
+++ app/src/main/java/it/bebra/cinema/presentation/main/activity/MainActivity.kt
@@ -1,10 +1,16 @@
 package it.bebra.cinema.presentation.main.activity
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import it.bebra.cinema.presentation.login.activity.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Затычка
+        startActivity(Intent(this, LoginActivity::class.java))
     }
 }
